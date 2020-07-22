c819288ca53ffeb6c56e7be11c6f69d23ef8e9e3
Nenouchka <helene.morel@gmail.com>
2020-07-22 20:08:17 +0200

    create landing page

--- src/components/Accueil.js
+++ src/components/Accueil.js
@@ -1,10 +1,40 @@
 import React from "react";
+import { Container, Row, Col, Image, Jumbotron } from 'react-bootstrap'
+import photo_ordi from '../assets/images/photo_ordi.jpg'
+import chateau from '../assets/images/chateau.jpg'
+import './Accueil.css'
+
 
 const Accueil = () => {
 
     return (
         <>
-            <p>Hello landing</p>
+            <Jumbotron className='txtCont' fluid>
+                <Container >
+                    <h1>Bienvenue sur mon portfolio !</h1>
+                    <br/>
+                    <p>
+                        Je m'appelle Hélène Morel.
+                        <br/>
+                        Passionnée de patrimoine et de digital, je vais bientôt allier les deux pendant mon stage chez 9b+.
+                    </p>
+                </Container>
+            </Jumbotron>
+            <Container className='acCont' fluid='xs'>
+                <Row >
+                    <Col xs={12} sm={6} md={6} >
+                        <div className='colAccueil'>
+                            <Image className='photo_ordi' src={photo_ordi} rounded fluid />   
+                        </div>
+                    </Col>
+                    <Col xs={12} sm={6} md={6} >
+                        <div className='colAccueil'>
+                            <Image className='photo_ordi' src={chateau} rounded fluid />   
+                        </div>
+                    </Col>
+                </Row>
+
+            </Container>
         </>   
     )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,21 @@
 import React from "react";
 import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
 import { Link } from "react-router-dom";
+import './Navbar.css'
 
 
 const Navb = () => {
 
     return (
         <>
-            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" className='navDiv'>
+            <Navbar collapseOnSelect expand="lg">
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav">
                 <Nav className="mr-auto" id='navDiv'>
-                    <Nav.Link id='nav-link' as={Link} to="/">Accueil</Nav.Link>
-                    <Nav.Link as={Link} to="/cv">CV</Nav.Link>
-                    <Nav.Link as={Link} to="/projets">Projets</Nav.Link>
-                    <Nav.Link as={Link} to="/interets">Centres d'intérêt</Nav.Link>
+                    <Nav.Link className='nav-link' as={Link} to="/">Accueil</Nav.Link>
+                    <Nav.Link className='nav-link' as={Link} to="/cv">CV</Nav.Link>
+                    <Nav.Link className='nav-link' as={Link} to="/projets">Projets</Nav.Link>
+                    <Nav.Link className='nav-link' as={Link} to="/interets">Centres d'intérêt</Nav.Link>
 
                 {/* <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
                     <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
